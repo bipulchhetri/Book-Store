f417b0c3e2b727d3f71d1717fd6bed1986a8b1a2
bipulchhetri <bipulchhetri80@gmail.com>
2025-05-13 23:16:04 +0530

    cart api done

--- backend/index.js
+++ backend/index.js
@@ -9,10 +9,14 @@ app.use(express.json());
 // Routes
 const userRoutes = require("./routes/user");
 const bookRoutes = require("./routes/book");
+const favourite=require("./routes/favourite")
+const cart=require("./routes/cart")
 
+// all API
 app.use("/api/users", userRoutes);  // All user routes under /api/users
 app.use("/api/books", bookRoutes);  // All book routes under /api/books
-
+app.use("/api/favourite",favourite);
+app.use("/api/cart",cart);
 // Root route
 app.get("/", (req, res) => {
     res.send("Hello Server");
